4170556134ca14968c1873026d1087b27a7462ec
Margaret Holmes <Margaret.Holmes@bcbsnc.com>
2017-04-25 12:15:04 -0400

    Updates for Enum

---
+++ AddressBook/MenuOption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace AddressBook
+{
+    public enum MenuOption              //when C# compiles, these all turn into numbers
+    {
+        Exit,
+        AddPerson,   //AddPerson = 200,  This will set it's number to 200, then next one will default to 201
+        AddCompany,
+        ListContacts,
+        SearchContacts,
+        RemoveContact
+        
+    }
+}
